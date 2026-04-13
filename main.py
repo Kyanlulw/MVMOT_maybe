bc632dcd3bdca6ac8680626a73f43c1ad7366cc2
Kyanlulw <datlc81@gmail.com>
2026-04-13 22:14:07 +0700

    change to cosine scheduler for smother loss, loss will decrease at epoch 25th

--- main.py
+++ main.py
@@ -59,6 +59,9 @@ def get_args_parser():
     parser.add_argument('--weight_decay', default=1e-4, type=float)
     parser.add_argument('--epochs', default=50, type=int)
     parser.add_argument('--lr_drop', default=40, type=int)
+    parser.add_argument('--lr_scheduler', default='step', type=str, choices=['step', 'cosine'])
+    parser.add_argument('--cosine_start_epoch', default=0, type=int,
+                        help='For cosine scheduler: keep LR flat until this epoch, then start cosine decay')
     parser.add_argument('--save_period', default=50, type=int)
     parser.add_argument('--lr_drop_epochs', default=None, type=int, nargs='+')
     parser.add_argument('--clip_max_norm', default=0.1, type=float,
@@ -344,7 +347,15 @@ def main(args):
     else:
         optimizer = torch.optim.AdamW(param_dicts, lr=args.lr,
                                       weight_decay=args.weight_decay)
-    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, args.lr_drop)
+    if args.lr_scheduler == 'cosine':
+        cosine_t_max = max(1, args.epochs - args.cosine_start_epoch)
+        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+            optimizer,
+            T_max=cosine_t_max,
+            eta_min=0.0,
+        )
+    else:
+        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, args.lr_drop)
 
     if args.distributed:
         model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu], find_unused_parameters=True)
@@ -388,7 +399,7 @@ def main(args):
             lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
             # todo: this is a hack for doing experiment that resume from checkpoint and also modify lr scheduler (e.g., decrease lr in advance).
             args.override_resumed_lr_drop = True
-            if args.override_resumed_lr_drop:
+            if args.override_resumed_lr_drop and args.lr_scheduler == 'step':
                 print('Warning: (hack) args.override_resumed_lr_drop is set to True, so args.lr_drop would override lr_drop in resumed lr_scheduler.')
                 lr_scheduler.step_size = args.lr_drop
                 lr_scheduler.base_lrs = list(map(lambda group: group['initial_lr'], optimizer.param_groups))
@@ -419,7 +430,11 @@ def main(args):
             sampler_train.set_epoch(epoch)
         train_stats = train_func(
             model, criterion, data_loader_train, optimizer, device, epoch, args.clip_max_norm)
-        lr_scheduler.step()
+        if args.lr_scheduler == 'cosine':
+            if (epoch + 1) >= args.cosine_start_epoch:
+                lr_scheduler.step()
+        else:
+            lr_scheduler.step()
 
         # Log train stats to wandb
         if args.wandb and HAS_WANDB and utils.is_main_process():
