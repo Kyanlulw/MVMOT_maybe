798b9a1528cd56b4a756a448bffd58d856c49fb3
Kyanlulw <datlc81@gmail.com>
2026-09-08 17:09:09 +0700

    Skip distributed launch for single GPU training

--- configs/r50_multiview_motr_train_wildtrack.sh
+++ configs/r50_multiview_motr_train_wildtrack.sh
@@ -78,4 +78,8 @@ if [ -n "$PRETRAIN" ]; then
 	set -- "$@" --pretrained "$PRETRAIN"
 fi
 
-"$PYTHON" -m torch.distributed.launch --nproc_per_node="$GPUS" --master_port="$PORT" --use_env main.py "$@"
+if [ "$GPUS" -eq 1 ]; then
+	"$PYTHON" main.py "$@"
+else
+	"$PYTHON" -m torch.distributed.launch --nproc_per_node="$GPUS" --master_port="$PORT" --use_env main.py "$@"
+fi
