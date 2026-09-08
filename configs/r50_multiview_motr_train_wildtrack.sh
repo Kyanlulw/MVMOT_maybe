d5d77995389988e7c8164f1d9cf7cae9c9009eb7
Kyanlulw <datlc81@gmail.com>
2026-09-08 16:58:53 +0700

    Make WildTrack launcher POSIX shell compatible

--- configs/r50_multiview_motr_train_wildtrack.sh
+++ configs/r50_multiview_motr_train_wildtrack.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+set -eu
 # Usage:
 #   bash configs/r50_multiview_motr_train_wildtrack.sh "0,1" /path/to/wildtrack_mvmot ./output/wildtrack_motr
 # If GPU_IDS is omitted, this script will use all visible GPUs.
@@ -31,13 +32,45 @@ fi
 export CUDA_VISIBLE_DEVICES="$GPU_IDS"
 export PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True"
 GPUS=$(echo "$GPU_IDS" | tr ',' '\n' | wc -l)
-PORT=$(( RANDOM % 1000 + 29500 ))
+PORT=${MASTER_PORT:-29500}
 echo "Using GPUs: $CUDA_VISIBLE_DEVICES (nproc_per_node=$GPUS)"
 echo "Pretrained checkpoint: ${PRETRAIN:-<none>}"
 
-PRETRAIN_ARGS=()
+set -- \
+    --meta_arch fusiontrack_motr \
+    --dataset_file e2e_mv_mot \
+    --epochs 80 \
+    --use_uncertainty_loss \
+    --use_reid_query \
+    --with_box_refine \
+    --lr 1e-5 \
+    --lr_backbone 1e-6 \
+    --output_dir "$OUTPUT_DIR" \
+    --batch_size 1 \
+    --sample_mode fixed_interval \
+    --sample_interval 1 \
+    --sampler_steps 20 \
+    --sampler_lengths 10 10 \
+    --update_query_pos \
+    --merger_dropout 0 \
+    --dropout 0 \
+    --random_drop 0.1 \
+    --fp_ratio 0.3 \
+    --query_interaction_layer QIM \
+    --extra_track_attn \
+    --use_checkpoint \
+    --mot_path "$MOT_PATH" \
+    --data_txt_path_train /kaggle/working/MVMOT_maybe/datasets/data_path/multiview_wildtrack.train \
+    --data_txt_path_val /kaggle/working/MVMOT_maybe/datasets/data_path/multiview_wildtrack.val \
+    --wandb \
+    --wandb_project fusiontrack_wildtrack \
+    --num_cams 7 \
+    --reid_num_layers 12 \
+    --reid_num_heads 6 \
+    --reid_tau1 30 \
+    --reid_tau2 10
 if [ -n "$PRETRAIN" ]; then
-	PRETRAIN_ARGS+=(--pretrained "$PRETRAIN")
+	set -- "$@" --pretrained "$PRETRAIN"
 fi
 
-python -m torch.distributed.launch --nproc_per_node="$GPUS" --master_port="$PORT" --use_env main.py --meta_arch fusiontrack_motr --dataset_file e2e_mv_mot --epochs 80 --use_uncertainty_loss --use_reid_query --with_box_refine --lr 1e-5 --lr_backbone 1e-6 "${PRETRAIN_ARGS[@]}" --output_dir "$OUTPUT_DIR" --batch_size 1 --sample_mode fixed_interval --sample_interval 1 --sampler_steps 20 --sampler_lengths 10 10 --update_query_pos --merger_dropout 0 --dropout 0 --random_drop 0.1 --fp_ratio 0.3 --query_interaction_layer QIM --extra_track_attn --use_checkpoint --mot_path "$MOT_PATH" --data_txt_path_train /kaggle/working/MVMOT_maybe/datasets/data_path/multiview_wildtrack.train --data_txt_path_val /kaggle/working/MVMOT_maybe/datasets/data_path/multiview_wildtrack.val --wandb --wandb_project fusiontrack_wildtrack --num_cams 7 --reid_num_layers 12 --reid_num_heads 6 --reid_tau1 30 --reid_tau2 10
+python -m torch.distributed.launch --nproc_per_node="$GPUS" --master_port="$PORT" --use_env main.py "$@"
